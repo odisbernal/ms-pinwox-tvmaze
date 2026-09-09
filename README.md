129a6b967bcd57d9c490dfce60569ccc0f349958
ocvillegas <odisbernalv@gmail.com>
2026-09-09 12:30:08 -0600

    Arquetipo inicial del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ms-pinwox-tvmaze
-Examen técnico Backend
+﻿# tvmaze
+
+Proyecto Spring Boot con Maven.
+
+Comandos:
+- `.\mvnw.cmd spring-boot:run`
+- `.\mvnw.cmd test`
+- `docker build -t tvmaze .`
---
+++ src/main/java/com/pinwox/tvmaze/TvMazeApplication.java
@@ -0,0 +1,11 @@
+package com.pinwox.tvmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvMazeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TvMazeApplication.class, args);
+    }
+}
